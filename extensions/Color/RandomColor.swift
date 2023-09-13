01693800e7005ff75bb3c495aa35fd330374df1b
Mike Kostenko <vopros.m@gmail.com>
2023-09-13 15:01:04 +0100

    Add random color generator

---
+++ extensions/Color/RandomColor.swift
@@ -0,0 +1,18 @@
+//
+//  RandomColor.swift
+//
+//  Created by Mike Kostenko on 13/09/2023.
+//
+
+import SwiftUI
+
+extension Color {
+    static var random: Color {
+        return Color(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1)
+        )
+    }
+}
+
